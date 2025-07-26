bb3c156c6a9f4c0a78428085e95e413a832886af
dada <dada@sf.com>
2025-07-26 20:13:26 +0300

    4 commit

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="product-list">
-    <div v-if="filteredProducts.length" class="products grid grid-cols-4 gap-2">
+    <div v-if="filteredProducts.length" class="products grid grid-cols-4 gap-5">
       <div
         v-for="product in filteredProducts"
         :key="product.id"
-        class="product-item bg-gray-100 p-3 rounded-lg"
+        class="product-item bg-white p-5 rounded-lg"
       >
         <NuxtLink :to="`/product/${product.id}`">
-          <img :src="`${product.image}`" />
+          <img
+            :src="`${product.image}`"
+            class="w-full h-56 object-contain block"
+          />
         </NuxtLink>
-        <h3 class="py-2">{{ product.name }}</h3>
-        <!-- <p>{{ product.description }}</p> -->
-        <p class="text-lg">{{ formatPrice(product.price) }} руб.</p>
-        <NuxtLink
-          :to="`/product/${product.id}`"
-          class="w-full bg-[#3f3f3f] text-center text-white block py-1 mt-5 ease-linear duration-150 hover:bg-[#6873ff]"
-          >Подробнее</NuxtLink
-        >
+        <div class="flex flex-col min-h-45">
+          <h3 class="py-2 text-base">{{ product.name }}</h3>
+          <p class="text-xl mt-auto mb-2">
+            {{ formatPrice(product.price) }} руб.
+          </p>
+          <NuxtLink
+            :to="`/product/${product.id}`"
+            class="w-full bg-[#3f3f3f] rounded-sm leading-[40px] text-center text-white block py-1 ease-linear duration-150 hover:bg-[#6873ff]"
+            >Конфигурировать</NuxtLink
+          >
+        </div>
         <!-- Ссылка на страницу товара -->
       </div>
     </div>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,62 @@
 <template>
   <div>
     <h1 class="text-3xl pb-8">Контакты</h1>
-    <div></div>
+    <div class="grid grid-cols-4 gap-4 pb-6">
+      <div class="flex bg-white p-4 rounded-xl flex-col gap-2">
+        <span class="italic text-[#4A5565]">Номер телефона</span>
+        <a href="tel:+78002227001" class="text-2xl">8 (800) 222-70-01</a>
+      </div>
+      <div class="flex bg-white p-4 rounded-xl flex-col gap-2">
+        <span class="italic text-[#4A5565]">Почта</span>
+        <a href="mailto:info@getmesite.ru" class="text-2xl"
+          >info@getmesite.ru</a
+        >
+      </div>
+      <div class="flex bg-white p-4 rounded-xl flex-col gap-2">
+        <span class="italic text-[#4A5565]">Telegram</span>
+        <a href="https://t.me/serverflow" class="text-2xl">t.me/serverflow</a>
+      </div>
+      <div class="flex bg-white p-4 rounded-xl flex-col gap-2">
+        <span class="italic text-[#4A5565]">WhatsApp</span>
+        <a href="wa.me/79934834956" class="text-2xl">wa.me/79934834956</a>
+      </div>
+    </div>
+    <div class="grid grid-cols-2 gap-6">
+      <div class="bg-white p-5 rounded-xl">
+        <h2 class="text-2xl pb-4">Москва</h2>
+        <a
+          href="https://yandex.ru/maps/-/CHTDB-iO"
+          class="text-lg"
+          target="_blank"
+          >ул. 2-я Брестская, 39 строение 4</a
+        >
+        <div class="rounded-md overflow-hidden mt-4">
+          <iframe
+            src="https://yandex.ru/map-widget/v1/?um=constructor%3A07bba5b2294432b736513b44ae37e55bacce151565cd698951384eedf64c1cd4&amp;source=constructor"
+            width="100%"
+            height="450"
+            frameborder="0"
+          ></iframe>
+        </div>
+      </div>
+      <div class="bg-white p-5 rounded-xl">
+        <h2 class="text-2xl pb-4">Санкт-Петербург</h2>
+        <a
+          href="https://yandex.ru/maps/-/CHTDFG-P"
+          class="text-lg"
+          target="_blank"
+          >ул. Заозёрная, 8, корп. 2Л</a
+        >
+        <div class="rounded-md overflow-hidden mt-4">
+          <iframe
+            src="https://yandex.ru/map-widget/v1/?um=constructor%3A3f35cad5e026a91022639dca19cde4ed641dd265029553ca2e38caffd8782b05&amp;source=constructor"
+            width="100%"
+            height="450"
+            frameborder="0"
+          ></iframe>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
